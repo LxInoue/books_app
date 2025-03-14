30c74a758dab5740655a356cb52cf966eecb3dc5
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-14 15:33:13 +0900

    7章の変更

--- Gemfile
+++ Gemfile
@@ -63,3 +63,5 @@ group :test do
 end
 
 gem "awesome_print"
+
+gem "carrierwave"
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -67,6 +67,6 @@ class BooksController < ApplicationController
     def book_params
       p "***************************"
       p params
-      params.expect(book: [ :title, :memo, :author ])
+      params.require(:book).permit(:title, :memo, :author, :picture)
     end
 end
